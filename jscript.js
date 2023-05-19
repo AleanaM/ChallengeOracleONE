10191705e2c29ceb834e8fea70b2df87076341dc
Alejandra Anahí Majín Martínez <31420370+AleanaM@users.noreply.github.com>
2023-05-19 11:26:43 -0600

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -108,9 +108,7 @@
 		function procesoDesencriptar(texto) {
 
 		    	return texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-
 		}
-
 
 
 
